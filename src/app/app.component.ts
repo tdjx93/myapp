d897342df2f61fe4adc7b3c8e6144b3aa73745ed
dawto <dawtor93@gmail.com>
2022-09-12 13:06:45 +0200

    content removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  users = [
+    {id: 1, name: "Dawid"},
+    {id: 2, name: "Jakub"},
+    {id: 3, name: "Mateusz"}
+  ];
+  events = [
+    {id: 1, name: "event 1", topic: "topic 1"},
+    {id: 2, name: "event 2", topic: "topic 2"},
+    {id: 3, name: "event 3", topic: "topic 3"},
+    {id: 4, name: "event 4", topic: "topic 4"}
+  ];
 }
+
