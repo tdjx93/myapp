5c17921d47861aeb21da1afe6a532bee77d185d4
lukasz <lukasz.usarz@gmail.com>
2022-09-21 10:51:36 +0200

    Refactor auth interceptor

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -8,13 +8,15 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
-    const authenticationRequest = request.clone(
-      {
-        headers: sessionStorage.getItem('token') ?
-          request.headers.set('Authorization', `Bearer ${sessionStorage.getItem('token')}`)
-          : request.headers.set('test', 'test')
-      }
-    );
-    return next.handle(authenticationRequest);
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      return next.handle(request.clone(
+        {
+          headers: request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      ));
+    } else {
+      return next.handle(request);
+    }
   }
 }
