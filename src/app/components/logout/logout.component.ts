5dcea92731548fbe97f3172ca9689bd076fb5492
dawto <dawtor93@gmail.com>
2022-09-21 09:08:13 +0200

    ctkj

---
+++ src/app/components/logout/logout.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss']
+})
+export class LogoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  logout() {
+
+  }
+
+}
--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -4,12 +4,17 @@ import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() {
+  }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${sessionStorage.getItem('token')}`)
-    });
-    return next.handle(authReq);
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    const authenticationRequest = request.clone(
+      {
+        headers: sessionStorage.getItem('token') ?
+          request.headers.set('Authorization', `Bearer ${sessionStorage.getItem('token')}`)
+          : request.headers.set('test', 'test')
+      }
+    );
+    return next.handle(authenticationRequest);
   }
 }
